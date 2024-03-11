093c01bc111dbf3696029f86d785602de80714ed
Pedro Tavano Ferreira <pedro_ferry@outlook.com>
2024-03-11 18:15:03 -0300

    adiciona projeto

---
+++ Divisao.java
@@ -0,0 +1,10 @@
+public class Divisao {
+    public static void main(String args[]) {
+      int x=125;
+      int y=25;
+      int w=-5;
+      int z=x/y/w;
+
+      System.out.println("o valor de x+y+w=" + z);
+    }
+}
---
+++ Subtracao.java
@@ -0,0 +1,10 @@
+public class Subtracao {
+    public static void main(String args[]) {
+      int x=125;
+      int y=25;
+      int w=-5;
+      int z=x-y-w;
+
+      System.out.println("o valor de x+y+w=" + z);
+    }
+}
