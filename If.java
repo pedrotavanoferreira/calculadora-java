2accbd31af0a00524979dd5234ac193d5f03b567
Pedro Tavano Ferreira <pedro_ferry@outlook.com>
2024-04-05 17:59:19 -0300

    adiciona if

---
+++ If.java
@@ -0,0 +1,10 @@
+public class If {
+    public static void main(String args[]) {
+        int x = 1;
+        if (x==0){
+            System.out.println("a variável é o número 0");
+        } else {
+            System.out.println("a variável não é o numero 0");
+        }
+    }
+}
