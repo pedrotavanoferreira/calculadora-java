ae6cc42fb08e5b6ac592c25f2fb8c01b6774f844
Pedro Tavano Ferreira <pedro_ferry@outlook.com>
2024-04-19 18:09:57 -0300

    adiciona entrada

--- If.java
+++ If.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class If {
-    public static void main(String args[]) {
-        int x = 0;
+    public static void main(String args[]) 
+    { 
+        Scanner entrada = new Scanner(System.in);
+        int x;
+        System.out.println("favor digitar um número:");
+        x = entrada.nextInt();
+        System.out.println("O número digitado é:" + x);
         if (x==0){
             System.out.println("a variável é o número 0");
         }else if (x==2){
