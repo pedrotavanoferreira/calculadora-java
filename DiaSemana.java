df9364eb7cd10fa00b88687c73b62df031a2fbfe
Pedro Tavano Ferreira <pedro_ferry@outlook.com>
2024-06-26 13:53:53 -0300

    adiciona projeto

---
+++ DiaSemana.java
@@ -0,0 +1,31 @@
+public class DiaSemana {
+    public static void main(String[] args)
+    {
+       for (int i = 1; i < 8; i++){
+        switch(i) {
+            case 1:
+              System.out.println("segunda");
+              break;
+            case 2:
+              System.out.println("terça");
+              break;
+            case 3:
+              System.out.println("quarta");
+              break;
+            case 4:
+              System.out.println("quinta");
+              break;
+            case 5:
+              System.out.println("sexta");
+              break;
+              case 6:
+              System.out.println("Sábado");
+              break;
+              case 7:
+              System.out.println("domingo");
+              break;
+            }
+        }
+    }
+}
+ 
